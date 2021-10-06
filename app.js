8771980ba48c82cfe6888ea6eedf9a040d39e20e
okoli praiz <okolipraiz.com>
2021-10-06 22:06:02 +0100

    Updated Project

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+// INIT GIHUB
+const github = new Github;
+
+// INIT UI   
+const ui = new UI;
+
 // SEARCH INPUT
-const seachUser = document.getElementById('searchUser');
+const searchUser = document.getElementById('searchUser');
 
 // SEARCH INPUT ADD EVENT LISTENER 
-
-seachUser.addEventListener('keyup', (e) => {
+searchUser.addEventListener('keyup', (e) => {
     //  GET INPUT TEXT
     const userText = e.target.value;
 
     if(userText !== ''){
-        console.log(userText);
+        // MAKE HTTP CALL
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === "Not found"){
+                // SHOW ALERT
+                ui.showAlert('User not found', 'alert alert-danger');  
+            }else{
+                //SHOW PROFILE
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        })
+    }else{
+        // CLEAR PROFILE
+        ui.clearProfile();
     }
-})
-//
+}); 
--- github.js
+++ github.js
@@ -1,5 +1,26 @@
-class GitHub{
+class Github{
     constructor(){
-        
+        this.client_id = '6ff970f8febef20de79a';
+        this.client_secret = '32f570f240b7bd1a8eebd2b004963a0b6d39ea95'
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch
+        (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
+        const repoResponse = await fetch
+        (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return{
+            profile,
+            repos
+        }
     }
 }
